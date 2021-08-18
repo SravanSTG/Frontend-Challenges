cedc9ad224efd65a677af4d63c20f630d3f3ae56
= <sravangaming7310@gmail.com>
2021-08-18 12:20:26 +0530

    FAQ Accordion card initial commit

---
+++ FAQ Accordion Card/index.js
@@ -0,0 +1,54 @@
+// Default on load
+// Make the first one bold and collapse all other
+$(".question1").addClass("bold");
+$("#arrow1").addClass("rotate");
+for (let i = 2; i <= 5; i++)
+{
+  $(".answer" + i).slideUp();
+}
+
+// On Click
+$(".arrow-down").click(function()
+  {
+    let arrowClass = this.id;
+    let arrowNumber = arrowClass.slice(arrowClass.length - 1);
+    removeQuestionBold();
+    questionBold(arrowNumber);
+    collapseAnswers(arrowNumber);
+    toggleCollapse(arrowNumber);
+  } );
+
+// Collapse all except the one clicked
+function collapseAnswers(ansNum)
+{
+  for (let i = 1; i <= 5; i++)
+  {
+    if (i !== ansNum)
+    {
+      $(".answer" + i).slideUp();
+      $("#arrow" + i).removeClass("rotate");
+    }
+  }
+}
+
+// Slide up and down on click
+function toggleCollapse(ansNum)
+{
+  $(".answer" + ansNum).slideToggle();
+  $("#arrow" + ansNum).addClass("rotate");
+}
+
+// Make question bold on click
+function questionBold(quesNum)
+{
+  $(".question" + quesNum).addClass("bold");
+}
+
+// Remove bold on all questions
+function removeQuestionBold()
+{
+  for (let i = 1; i <= 5; i++)
+  {
+    $(".question" + i).removeClass("bold");
+  }
+}
